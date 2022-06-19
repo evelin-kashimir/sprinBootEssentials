0b38e5ab82fb1439be71ad8c2f85e9c70014981a
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-06-19 17:10:19 -0300

    [SpringBoot] - ExHandler - Personalizando os detalhes da exceção

--- springBoot2-essentials/src/main/java/academy/devdojo/springboot2/exception/BadRequestException.java
+++ springBoot2-essentials/src/main/java/academy/devdojo/springboot2/exception/BadRequestException.java
@@ -1,9 +1,9 @@
 package academy.devdojo.springboot2.exception;
 
 //CLASSE PRA TRATAR EXCEÇÕES DE ERROS CUSTOMIZADAS
+import lombok.Builder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException{
---
+++ springBoot2-essentials/src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
@@ -0,0 +1,28 @@
+package academy.devdojo.springboot2.handler;
+
+//PERSONALIZANDO OS DETALHES DA BAD EXEPCTION
+import academy.devdojo.springboot2.exception.BadRequestException;
+import academy.devdojo.springboot2.exception.BadRequestExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+//Será utilizado em todos os End Points do Controller
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre){
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Xii, deu ruim aqui")
+                        .details(bre.getMessage())
+                        .developerMessage(bre.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST
+        );
+    }
+}
